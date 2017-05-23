2ca8254013a1eafeda8448ce7f7a87a46898301a
riyasinha875 <riyasinha875@gmail.com>
2017-05-23 15:29:15 +0530

    initialupload

---
+++ src/test/java/riyaproject/AppTest.java
@@ -0,0 +1,15 @@
+package riyaproject;
+
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+	@Test
+public void sendmail()
+{
+	System.out.println("****sent****");
+}
+}
